2e7b474dec62fd2c2b58019349e0d052173fe56a
Orhun İPEK <130233457+iambl0ck@users.noreply.github.com>
2024-08-25 16:32:15 +0300

    v0.1

---
+++ Quest App/sinav.js
@@ -0,0 +1,107 @@
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+// Sorularını buraya manuel olarak ekleyebilirsin
+let questions = [
+    {
+        question: "Yavuzlar takımı hangi alanda uzmanlaşmıştır?",
+        choice1: "Web Güvenliği ve Red Team",
+        choice2: "Yapay Zeka",
+        choice3: "Mobil Uygulama Geliştirme",
+        choice4: "Zararlı Yazılım Analizi",
+        answer: 1
+    },
+    {
+        question: "Siber Abi kimdir",
+        choice1: "Mozart",
+        choice2: "William Shakespeare",
+        choice3: "Muhammed Temli",
+        choice4: "Jane Austen",
+        answer: 3
+    },
+    {
+        question: "What is the smallest planet in our solar system?",
+        choice1: "Earth",
+        choice2: "Mars",
+        choice3: "Venus",
+        choice4: "Mercury",
+        answer: 4
+    }
+];
+
+//Puanlar burda
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
+};
+
+
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
+        return window.location.assign('/end.html');
+    }
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerHTML = currentQuestion.question;
+
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerHTML = currentQuestion['choice' + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+        const classToApply =
+            selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        if (classToApply === 'correct') {
+            incrementScore(CORRECT_BONUS);
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+    });
+});
+
+incrementScore = (num) => {
+    score += num;
+    scoreText.innerText = score;
+};
+
+startGame();
