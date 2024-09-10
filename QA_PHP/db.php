5e199cf4955a1678f5eabb3c9908511eccb28851
Orhun İPEK <130233457+iambl0ck@users.noreply.github.com>
2024-09-10 13:47:46 +0300

    Update db.php

--- QA_PHP/db.php
+++ QA_PHP/db.php
@@ -1,10 +1,8 @@
 <?php
-// Veritabanı dosyasına bağlanma
-$dsn = 'sqlite:/questionapp.db';
 
 try {
     // SQLite PDO bağlantısı
-    $db = new PDO($dsn);
+    $db = new PDO("sqlite:questionapp.db");
     // Hataları yakalama modu
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
